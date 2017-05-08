0f163d3089a824008a40da69033636acc7731376
Ganeshwara Herawan Hananda <ghananda@noon.com>
2017-05-08 08:04:50 +0000

    adding color

--- src/main/java/com/lolski/kafka/MessagesKafkaProducer.java
+++ src/main/java/com/lolski/kafka/MessagesKafkaProducer.java
@@ -1,11 +1,13 @@
 package com.lolski.kafka;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.messaging.support.GenericMessage;
+import com.lolski.AnsiColor;
+
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+//import org.springframework.cloud.stream.messaging.Source;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.integration.annotation.InboundChannelAdapter;
+//import org.springframework.messaging.support.GenericMessage;
 //
 //@Configuration
 //@EnableBinding(Source.class)
@@ -13,11 +15,11 @@ public class MessagesKafkaProducer {
   // produce
 
   public void send(MessageCreated messageCreated) {
-    System.out.println("received a new message '" + messageCreated.getText() + "' with id '" + messageCreated.getId() + "'");
+    System.out.println(AnsiColor.ANSI_YELLOW + "received a new message '" + messageCreated.getText() + "' with id '" + messageCreated.getId() + "'" + AnsiColor.ANSI_RESET);
   }
 
   public void send(MessageRead messageRead) {
-    System.out.println("message " + messageRead.getId() + " marked as read");
+    System.out.println(AnsiColor.ANSI_YELLOW + "message " + messageRead.getId() + " marked as read" + AnsiColor.ANSI_RESET);
   }
 
 }
